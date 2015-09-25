ec76ae3c3cb63dc655a1959133a949ac5e497940
David Tisdall <david.tisdall@katasan.com>
2015-09-25 14:45:26 -0400

    add eateries

--- concessions.rb
+++ concessions.rb
@@ -9,8 +9,8 @@ class Concession
 	end
 
 	def parse
-		xml_obj = ActiveSupport::XmlMini.parse(open('http://www.nycgovparks.org/bigapps/DPR_Concessions_001.xml'))
-		xml_obj["root"]["concession"].select{|item| item["park_id"]["__content__"] == @park_id}.map{|found| found["Name"]["__content__"]}
+		xml_obj = ActiveSupport::XmlMini.parse(open('http://www.nycgovparks.org/bigapps/DPR_Eateries_001.xml'))
+		xml_obj["eateries"]["facility"].select{|item| item["park_id"]["__content__"] == @park_id}.map{|found| found["Name"]["__content__"]}
 	end
 
 	def summary 
